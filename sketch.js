b6b98d9aebe2bad9c34c7674a7ffd27c509442f9
marydeepna <71275492+marydeepna@users.noreply.github.com>
2020-09-22 11:19:16 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,28 @@
+var fr,mr;
+function setup(){
+    createCanvas(800,800);
+    fr=createSprite(200,200,200,100);
+    fr.shapeColor="green";
+    mr= createSprite(500,200,200,100);
+      fr.shapeColor="green";
+
+      mr.velocityY = 5;
+      fr.velocityY =-5;
+}
+function draw(){
+background("yellow");
+
+
+if(mr.x-fr.x < mr.width/2  +  fr.width/2 && fr.x-mr.x < mr.width/2  +  fr.width/2){
+  mr.velocityX = -mr.velocityX 
+  fr.velocityX = -fr.velocityX 
+}
+  if(mr.y-fr.y < mr.height/2  +  fr.height/2||fr.y-mr.y < mr.height/2  +  fr.height/2){
+    mr.velocityY = -mr.velocityY 
+    fr.velocityY = -fr.velocityY 
+}
+
+
+drawSprites();
+
+}
